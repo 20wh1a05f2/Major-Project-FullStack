0ba5feaccfd3fd0056067708d890af17010718ab
Ananthula Varshitha <83571478+20wh1a05f2@users.noreply.github.com>
2022-11-27 17:11:53 +0530

    index.js file with register and login routes

--- index.js
+++ index.js
@@ -2,17 +2,75 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore} = require('firebase-admin/firestore');
+
+var serviceAccount = require("./key.json");
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+const db = getFirestore();
+
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  res.send('Hello World! 1233')
+  res.render('main.ejs');
 })
-app.get('/signup', (req, res) => {
-  res.render('signup');
+
+app.get('/register', (req, res) => {
+  res.render('signup.ejs');
 })
-app.get('/signin', (req, res) => {
-  res.render('signin');
+app.get('/login', (req, res) => {
+  res.render('signin.ejs');
+})
+
+app.get('/signinsubmit',(req, res)=>{
+  const Email = req.query.Email;
+  const Password = req.query.Password;
+
+  db.collection("users")
+    .where("Email", "==", Email)
+    .where("Password", "==", Password)
+    .get()
+    .then((docs)=>{
+      if(docs.size>0){
+        var usersData = [];
+        db.collection("users")
+          .get()
+          .then((docs)=>{
+            docs.forEach((doc)=>{
+              usersData.push(doc.data());
+            })
+        })
+        .then(()=>{
+          res.render("dashboard.ejs",{userData: usersData})
+        })
+        
+      }
+      else{
+        res.send("Invalid Login")
+      }
+    })
+
+})
+
+app.get('/signupsubmit',(req, res)=>{
+  const FullName = req.query.FullName;
+  const RollNumber = req.query.RollNumber;
+  const Email = req.query.Email;
+  const Password = req.query.Password;
+  const Confirm = req.query.ConfirmPassword;
+  db.collection("users").add({
+    Name: FullName,
+    RollNumber: RollNumber,
+    Email: Email,
+    Password: Password,
+    Confirm: Confirm,
+}).then(()=>{
+  res.render("success.ejs");
+});
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
